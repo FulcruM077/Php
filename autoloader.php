3ece92c1e7795789a331c83a1ed1460fcbdb5e1c
Can AYDIN <bcan34@hotmail.com>
2020-12-01 16:33:30 +0300

    Create autoloader.php

---
+++ autoloader.php
@@ -0,0 +1,18 @@
+<?php
+# FulcruM
+
+# Defined APP path for ease of use
+define('APP', $_SERVER['DOCUMENT_ROOT'] . '/app');
+
+# Define Project Namespace tag
+define('PNAMESPACE', 'FulcruM\\');
+
+spl_autoload_register(function($class) {
+	$file = APP . '/'. $class . '.php';
+	$file = str_replace(PNAMESPACE, '', $file);
+	$file = str_replace('\\', DIRECTORY_SEPARATOR, $file);
+      echo $file;
+	if (file_exists($file)) {
+		require_once($file);
+	}
+});
